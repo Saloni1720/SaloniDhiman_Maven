6c7d9299ea47bf4f92a13cda5f01175b1c2ca5ab
Saloni_Dhiman <saloni1720@gmail.com>
2020-02-14 00:05:27 +0530

    Adding gift numbers

--- src/main/java/com/epam/firstmavenproject/Main.java
+++ src/main/java/com/epam/firstmavenproject/Main.java
@@ -31,13 +31,28 @@ public class Main {
     public static void main(String[] args) {
 	List<Gift> gifts = new ArrayList<>();
 	String[] availableSweets = new String[] {"Jelly", "Chocolate", "Toffee"};
-	while(gifts.size() != 1) {
+	while(gifts.size() != 5) {
 	    Gift gift = new Gift();
 	    gifts.add(gift);
 	    putSweetsRandomly(gift, availableSweets);
 	}
-	gifts.forEach(gift -> gift.sortByPrice());
-	gifts.forEach(gift -> gift.sortByWeight());
+	System.out.println("Sweets in the gifts before sorting:");
+	gifts.forEach(gift -> {
+	    System.out.println("\nGift Number: " + new Integer(gift.hashCode()).toString().substring(0, 4) + "\n");
+	    gift.openGift().forEach(sweet -> System.out.println(sweet.getClass().getSimpleName()));
+	    });
+	System.out.println("Sweets in the gifts after sorting by price:");
+	gifts.forEach(gift -> {
+	    gift.sortByPrice();
+	    System.out.println("\nGift Number: " + new Integer(gift.hashCode()).toString().substring(0, 4) + "\n");
+	    gift.openGift().forEach(sweet -> System.out.println(sweet.getClass().getSimpleName()));
+	    });
+	System.out.println("Sweets in the gifts after sorting by weight:");
+	gifts.forEach(gift -> {
+	    gift.sortByWeight();
+	    System.out.println("\nGift Number: " + new Integer(gift.hashCode()).toString().substring(0, 4) + "\n");
+	    gift.openGift().forEach(sweet -> System.out.println(sweet.getClass().getSimpleName()));
+	    });
     }
 
 }
